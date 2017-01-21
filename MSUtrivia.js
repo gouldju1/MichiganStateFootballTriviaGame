aff9f3f5572c023ac6ffcee7cf40298f55b59798
Madison Heise <madisonheise@Madisons-MacBook-Pro-2.local>
2017-01-21 18:46:52 -0500

    add code

---
+++ MSUtrivia.js
@@ -0,0 +1,511 @@
+'use strict';
+var questions = [
+    {
+        "What coach has won the most national championships?": [
+            "Duffy Daugherty",
+            "Mark Dantonio",
+            "Biggie Munn",
+            "George Perles"
+        ]
+    },
+    {
+        "What year was the first MSU football seaon?": [
+            "1896",
+            "1876",
+            "1886",
+            "1866"
+        ]
+    },
+    {
+        "What is the seating capacity of Spartan Stadium?": [
+            "75,005",
+            "65,005",
+            "55,000",
+            "80,000"
+        ]
+    },
+    {
+        "When was the Spartan Marching Band established?": [
+            "1870",
+            "1855",
+            "1896",
+            "1900"
+        ]
+    },
+    {
+        "What year did Michigan State join the Big Ten?": [
+            "1950",
+            "1955",
+            "1960",
+            "1965"
+        ]
+    },
+    {
+        "What was Michigan State Football Team's name before Spartans?": [
+            "Aggies",
+            "Red Cedar Fighters",
+            "Warriors",
+            "Statesmen"
+        ]
+    },
+    {
+        "What year did Michigan State Football change their team name to Spartans?": [
+            "1925",
+            "1918",
+            "1944",
+            "1950"
+        ]
+    },
+    {
+        "How much does the current Sparty costume cost?": [
+            "$8,000",
+            "$4,000",
+            "$1,000",
+            "$13,000"
+        ]
+    },
+    {
+        "How much does Sparty's costume weigh?": [
+            "30 pounds",
+            "20 pounds",
+            "10 pounds",
+            "40 pounds"
+        ]
+    },
+    {
+        "Who was the first coach of the MSU Football Team?": [
+            "Henry Keep",
+            "Frank Sommers",
+            "Harry Kipke",
+            "Charles Bachman"
+        ]
+    },
+    {
+        "Who coached the MSU Football team in 1896?": [
+            "No coach in 1896",
+            "Charles Bachman",
+            "Harry Kipke",
+            "Henry Keep"
+        ]
+    },
+    {
+        "The Spartan Marching Band has how many members?": [
+            "300",
+            "250",
+            "200",
+            "350"
+        ]
+    },
+    {
+        "Who is the winningest coach in MSU Football history?": [
+            "John Macklin",
+            "Mark Dantonio",
+            "Biggie Munn",
+            "Duffy Daugherty"
+        ]
+    },
+    {
+        "Who, as of January 21, 2017, is the current MSU Athletic Director?": [
+            "Mark Hollis",
+            "Tom Izzo",
+            "Glen Bennett",
+            "Morris Watts"
+        ]
+    },
+    {
+        "What was Spartan Stadium originally called?": [
+            "College Field",
+            "Aggies Stadium",
+            "Macklin Field",
+            "Spartan Field"
+        ]
+    },
+    {
+        "How many times have the Spartans won the Rose Bowl?": [
+            "Four",
+            "Three",
+            "Two",
+            "One"
+        ]
+    },
+    {
+        "How many Big Ten Championhips have the Spartans won?": [
+            "Nine",
+            "Five",
+            "Ten",
+            "Four"
+        ]
+    },
+    {
+        "What is the original Sparty Statue made from?": [
+            "Terra Cotta",
+            "Bronze",
+            "Gold",
+            "Copper"
+        ]
+    },
+    {
+        "Who was the University of Michigan punter who fumbled the ball causing Michigan State to win the rival game in 2015?": [
+            "Blake O'Neill",
+            "Jabrill Peppers",
+            "Jake Butt",
+            "Amara Darboh"
+        ]
+    },
+    {
+        "What is the only Michigan State Football jersey number to be retired, which did not belong to a player?": [
+            "46",
+            "30",
+            "55",
+            "96"
+        ]
+    },
+];
+
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+}
+
+
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    getWelcomeResponse(callback);
+}
+
+
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    if (session.attributes && session.attributes.userPromptedToContinue) {
+        delete session.attributes.userPromptedToContinue;
+        if ("AMAZON.NoIntent" === intentName) {
+            handleFinishSessionRequest(intent, session, callback);
+        } else if ("AMAZON.YesIntent" === intentName) {
+            handleRepeatRequest(intent, session, callback);
+        }
+    }
+
+    if ("AnswerIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AnswerOnlyIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("DontKnowIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.YesIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.NoIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.StartOverIntent" === intentName) {
+        getWelcomeResponse(callback);
+    } else if ("AMAZON.RepeatIntent" === intentName) {
+        handleRepeatRequest(intent, session, callback);
+    } else if ("AMAZON.HelpIntent" === intentName) {
+        handleGetHelpRequest(intent, session, callback);
+    } else if ("AMAZON.StopIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else if ("AMAZON.CancelIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else {
+        throw "Invalid intent";
+    }
+}
+
+
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+}
+
+
+var ANSWER_COUNT = 4;
+var GAME_LENGTH = 5;
+var CARD_TITLE = "Trivia"; 
+
+function getWelcomeResponse(callback) {
+    var sessionAttributes = {},
+        speechOutput = "I will ask you " + GAME_LENGTH.toString()
+            + " questions, try to get as many right as you can. Just say the number of the answer. Let's begin. ",
+        shouldEndSession = false,
+
+        gameQuestions = populateGameQuestions(),
+        correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT)), 
+        roundAnswers = populateRoundAnswers(gameQuestions, 0, correctAnswerIndex),
+
+        currentQuestionIndex = 0,
+        spokenQuestion = Object.keys(questions[gameQuestions[currentQuestionIndex]])[0],
+        repromptText = "Question 1. " + spokenQuestion + " ",
+
+        i, j;
+
+    for (i = 0; i < ANSWER_COUNT; i++) {
+        repromptText += (i+1).toString() + ". " + roundAnswers[i] + ". "
+    }
+    speechOutput += repromptText;
+    sessionAttributes = {
+        "speechOutput": repromptText,
+        "repromptText": repromptText,
+        "currentQuestionIndex": currentQuestionIndex,
+        "correctAnswerIndex": correctAnswerIndex + 1,
+        "questions": gameQuestions,
+        "score": 0,
+        "correctAnswerText":
+            questions[gameQuestions[currentQuestionIndex]][Object.keys(questions[gameQuestions[currentQuestionIndex]])[0]][0]
+    };
+    callback(sessionAttributes,
+        buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, shouldEndSession));
+}
+
+function populateGameQuestions() {
+    var gameQuestions = [];
+    var indexList = [];
+    var index = questions.length;
+
+    if (GAME_LENGTH > index){
+        throw "Invalid Game Length.";
+    }
+
+    for (var i = 0; i < questions.length; i++){
+        indexList.push(i);
+    }
+
+    for (var j = 0; j < GAME_LENGTH; j++){
+        var rand = Math.floor(Math.random() * index);
+        index -= 1;
+
+        var temp = indexList[index];
+        indexList[index] = indexList[rand];
+        indexList[rand] = temp;
+        gameQuestions.push(indexList[index]);
+    }
+
+    return gameQuestions;
+}
+
+function populateRoundAnswers(gameQuestionIndexes, correctAnswerIndex, correctAnswerTargetLocation) {
+   
+    var answers = [],
+        answersCopy = questions[gameQuestionIndexes[correctAnswerIndex]][Object.keys(questions[gameQuestionIndexes[correctAnswerIndex]])[0]],
+        temp, i;
+
+    var index = answersCopy.length;
+
+    if (index < ANSWER_COUNT){
+        throw "Not enough answers for question.";
+    }
+
+    for (var j = 1; j < answersCopy.length; j++){
+        var rand = Math.floor(Math.random() * (index - 1)) + 1;
+        index -= 1;
+
+        var temp = answersCopy[index];
+        answersCopy[index] = answersCopy[rand];
+        answersCopy[rand] = temp;
+    }
+
+    for (i = 0; i < ANSWER_COUNT; i++) {
+        answers[i] = answersCopy[i];
+    }
+    temp = answers[0];
+    answers[0] = answers[correctAnswerTargetLocation];
+    answers[correctAnswerTargetLocation] = temp;
+    return answers;
+}
+
+function handleAnswerRequest(intent, session, callback) {
+    var speechOutput = "";
+    var sessionAttributes = {};
+    var gameInProgress = session.attributes && session.attributes.questions;
+    var answerSlotValid = isAnswerSlotValid(intent);
+    var userGaveUp = intent.name === "DontKnowIntent";
+
+    if (!gameInProgress) {
+        
+        sessionAttributes.userPromptedToContinue = true;
+        speechOutput = "There is no game in progress. Do you want to start a new game? ";
+        callback(sessionAttributes,
+            buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, false));
+    } else if (!answerSlotValid && !userGaveUp) {
+       
+        var reprompt = session.attributes.speechOutput;
+        var speechOutput = "Your answer must be a number between 1 and " + ANSWER_COUNT + ". " + reprompt;
+        callback(session.attributes,
+            buildSpeechletResponse(CARD_TITLE, speechOutput, reprompt, false));
+    } else {
+        var gameQuestions = session.attributes.questions,
+            correctAnswerIndex = parseInt(session.attributes.correctAnswerIndex),
+            currentScore = parseInt(session.attributes.score),
+            currentQuestionIndex = parseInt(session.attributes.currentQuestionIndex),
+            correctAnswerText = session.attributes.correctAnswerText;
+
+        var speechOutputAnalysis = "";
+
+        if (answerSlotValid && parseInt(intent.slots.Answer.value) == correctAnswerIndex) {
+            currentScore++;
+            speechOutputAnalysis = "correct. ";
+        } else {
+            if (!userGaveUp) {
+                speechOutputAnalysis = "wrong. "
+            }
+            speechOutputAnalysis += "The correct answer is " + correctAnswerIndex + ": " + correctAnswerText + ". ";
+        }
+        if (currentQuestionIndex == GAME_LENGTH - 1) {
+            speechOutput = userGaveUp ? "" : "That answer is ";
+            speechOutput += speechOutputAnalysis + "You got " + currentScore.toString() + " out of "
+                + GAME_LENGTH.toString() + " questions correct. Thank you for playing!";
+            callback(session.attributes,
+                buildSpeechletResponse(CARD_TITLE, speechOutput, "", true));
+        } else {
+            currentQuestionIndex += 1;
+            var spokenQuestion = Object.keys(questions[gameQuestions[currentQuestionIndex]])[0];
+            // Generate a random index for the correct answer, from 0 to 3
+            correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
+            var roundAnswers = populateRoundAnswers(gameQuestions, currentQuestionIndex, correctAnswerIndex),
+
+                questionIndexForSpeech = currentQuestionIndex + 1,
+                repromptText = "Question " + questionIndexForSpeech.toString() + ". " + spokenQuestion + " ";
+            for (var i = 0; i < ANSWER_COUNT; i++) {
+                repromptText += (i+1).toString() + ". " + roundAnswers[i] + ". "
+            }
+            speechOutput += userGaveUp ? "" : "That answer is ";
+            speechOutput += speechOutputAnalysis + "Your score is " + currentScore.toString() + ". " + repromptText;
+
+            sessionAttributes = {
+                "speechOutput": repromptText,
+                "repromptText": repromptText,
+                "currentQuestionIndex": currentQuestionIndex,
+                "correctAnswerIndex": correctAnswerIndex + 1,
+                "questions": gameQuestions,
+                "score": currentScore,
+                "correctAnswerText":
+                    questions[gameQuestions[currentQuestionIndex]][Object.keys(questions[gameQuestions[currentQuestionIndex]])[0]][0]
+            };
+            callback(sessionAttributes,
+                buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, false));
+        }
+    }
+}
+
+function handleRepeatRequest(intent, session, callback) {
+    
+    if (!session.attributes || !session.attributes.speechOutput) {
+        getWelcomeResponse(callback);
+    } else {
+        callback(session.attributes,
+            buildSpeechletResponseWithoutCard(session.attributes.speechOutput, session.attributes.repromptText, false));
+    }
+}
+
+function handleGetHelpRequest(intent, session, callback) {
+    
+    if (!session.attributes) {
+        session.attributes = {};
+    }
+
+    session.attributes.userPromptedToContinue = true;
+
+
+    var speechOutput = "I will ask you " + GAME_LENGTH + " multiple choice questions. Respond with the number of the answer. "
+        + "For example, say one, two, three, or four. To start a new game at any time, say, start game. "
+        + "To repeat the last question, say, repeat. "
+        + "Would you like to keep playing?",
+        repromptText = "To give an answer to a question, respond with the number of the answer . "
+        + "Would you like to keep playing?";
+        var shouldEndSession = false;
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession));
+}
+
+function handleFinishSessionRequest(intent, session, callback) {
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Good bye!", "", true));
+}
+
+function isAnswerSlotValid(intent) {
+    var answerSlotFilled = intent.slots && intent.slots.Answer && intent.slots.Answer.value;
+    var answerSlotIsInt = answerSlotFilled && !isNaN(parseInt(intent.slots.Answer.value));
+    return answerSlotIsInt && parseInt(intent.slots.Answer.value) < (ANSWER_COUNT + 1) && parseInt(intent.slots.Answer.value) > 0;
+}
+
+
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
